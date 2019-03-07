98cc627e6a66e37c6f0f9f146835928c41344989
calvinschmidt4 <calvinschmidt4@users.noreply.github.com>
2019-03-07 17:58:38 +0100

    Update tijd.js

--- javascript/tijd.js
+++ javascript/tijd.js
@@ -1,52 +1,65 @@
-var d,h,m,s,animate;
+var date, h, m, s, count;
 
-function time(){
-    d=new Date();
-    h=d.getHours();
-    m=d.getMinutes();
-    s=d.getSeconds();
+function load(){
     clock();
     background();
-};
-
-function clock(){;
-    s++;
-    if(s==60){
-        s=0;
-        m++;
-        if(m==60){
-            m=0;
-            h++;
-            if(h==24){
-                h=0;
-            }
-        }    
-    }
-    $('sec',s);
-    $('min',m);
-    $('hr',h);
-    background();
-    animate=setTimeout(clock,1000);
-};
-function $(id, val){
-    if(val<10){
-        val='0'+val;
-    }
-    document.getElementById(id).innerHTML=val;
+    zeroAdded();
+    riseFall();      
+}    
+  
+function clock(){
+    date = new Date();
+    // de huidige uren, minuten en secondes worden opgevraagd.
+    h = date.getHours();
+    m = date.getMinutes();
+    s = date.getSeconds();
+    // de opgevraagde tijden worden gekoppeld aan de functie zeroAdded(i).
+    h = zeroAdded(h);
+    m = zeroAdded(m);
+    s = zeroAdded(s);    
     
-};
+    // de vorige waardes worden vervolgens in de div met id clock geplaatst.
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    // background();
+    count=setTimeout(clock,1000);
+}
+// de opgevraagde tijden worden gecontroleerd, wanneer een uur, minuut of seconde kleiner is dan 10 wordt daar een 0 aan vastgeplakt, zodat de klok er als een echte digitale klok uitziet.
+function zeroAdded(i) {
+    if (i < 10){
+        i = "0" + i};
+        return i;
+}
 
-function background(){
-    if(h > 19 || h < 6){
-        document.body.className = "night";
+// // de achtergrond verandert afhankelijk van hoe laat het is. Dit gebeurt alleen op uur niveau. Bij een bepaald tijdstip wordt er aan body een class toegevoegd.
+// function background(){
+//     if(h > 19 || h < 6){
+//         document.body.className = "night";
         
-    } else if (h > 16 && h < 19){
-        document.body.className = "sunset";
-    } else {
-        document.body.className = "day";
-        document.getElementById("tekst").innerHTML = 'Day';
+//     } else if (h > 16 && h < 19){
+//         document.body.className = "sunset";
+//     } else {
+//         document.body.className = "day";
+//         document.getElementById("tekst").innerHTML = 'Day';
         
+//     }
+// };
+
+function riseFall() {
+    if (h > 17 && h < 20){
+        document.body.className = "sunfall";
     }
-};
+}
 
-window.onload = time;
+
+load();
+
+
+
+
+
+
+
+
+
+
+
