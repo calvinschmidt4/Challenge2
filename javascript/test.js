94771e04752577597ca738259a5580f1d0c78550
calvinschmidt4 <calvinschmidt4@users.noreply.github.com>
2019-03-14 16:33:00 +0100

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -1,11 +1,6 @@
 var date, h, m, s, count;
 
-function load(){
-    clock();    
-    zeroAdded();
-    Animate(); 
-}    
-  
+
 function clock(){
     date = new Date();
     // de huidige uren, minuten en secondes worden opgevraagd.
@@ -20,7 +15,8 @@ function clock(){
     // de vorige waardes worden vervolgens in de div met id clock geplaatst.
     document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
     // background();
-    Animate(); 
+    message()
+    animate(); 
     count=setTimeout(clock,1000);
 }
 // de opgevraagde tijden worden gecontroleerd, wanneer een uur, minuut of seconde kleiner is dan 10 wordt daar een 0 aan vastgeplakt, zodat de klok er als een echte digitale klok uitziet.
@@ -28,64 +24,71 @@ function zeroAdded(i) {
     if (i < 10){
         i = "0" + i};
         return i;
-}
+};
 
-function Animate() {
-    // maakt een variabele aan met de id sun    
-    var sunAnimate = document.getElementById("sun");  
+// maakt een variabele aan met de id sun  
+var sunAnimate = document.getElementById("sun");
+
+function animate() {         
     // op basis van hoe laat het is wordt berekent hoeveel de zon zich moet verplaatsen 
-    var moveValueUp = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (80 / 7);
-    var moveValueDown = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 13) * (80 / 7);
-    var moveValueRight = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (80 / 14);
+    var moveValueUp = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (100 / 7);
+    var moveValueDown = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 13) * (100 / 7);
+    var moveValueRight = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (100 / 14);
     console.log(moveValueUp);
     // als tijd tussen onderstaande uren is dan wordt de zon verplaatst over de afstand van moveValue in percentages
     // als de tijd buiten die uren is dan wordt er een class toegevoegd waarmee de zon op display: none wordt gezet.
-      
+       
     if (h >= 6 && h < 13){
-        TweenMax.to(sunAnimate, 1, {bottom: moveValueUp += '%', left: moveValueRight += '%'});
+        TweenMax.to(sunAnimate, 0, {bottom: moveValueUp += '%', left: moveValueRight += '%'});
     }
 
     else if (h >= 13 && h < 20){
-        TweenMax.to(sunAnimate, 1, {top: moveValueDown += '%', left: moveValueRight += '%'});
+        TweenMax.to(sunAnimate, 0, {top: moveValueDown += '%', left: moveValueRight += '%'});
     }
-    
-    
-    
-    
-    // if (h >= 6 && h <20){        
-        
-    //     TweenMax.to(sunAnimate, 1, {bottom: moveValueUp += '%'});
-        
-    //     if (h >= 6 && h <8){
-    //         document.getElementById("tekst").innerHTML = 'Sunrise';
-    //     } 
-    //     else if (h >= 8 && h <18){
-    //         document.getElementById("tekst").innerHTML = 'Day';   
-    //     }
-    //     else if (h >= 18 && h <20){
-    //         document.getElementById("tekst").innerHTML = 'Sunset';
-    //     }        
-    //     else {
-    //         document.getElementById("tekst").innerHTML = 'Night';
-    //     }        
-    // } 
-    // else {
-    //     var image = document.getElementById("sun");
-    //     image.classList.add("sun_remove");
-    // }
-}
+          
+    else {
+    var image = document.getElementById("sun");
+    image.classList.add("sun_remove");
+    var backgroundColor = document.getElementById("change_color");
+    backgroundColor.classList.add("night");
+    }
+};
 
-load();
+function message(){                            
+    if (h >= 6 && h <8){
+        document.getElementById("tekst").innerHTML = 'Sunrise';
+    } 
+    else if (h >= 8 && h <18){
+        document.getElementById("tekst").innerHTML = 'Day';   
+    }
+    else if (h >= 18 && h <20){
+        document.getElementById("tekst").innerHTML = 'Sunset';
+    }        
+    else {
+        document.getElementById("tekst").innerHTML = 'Night';
+    }        
 
-// function Animate() {
-//     var sunAnimate = document.getElementById("sun");    
-//     var tl1 = new TimelineMax({repeat:1});
-//     tl1.to(sunAnimate, 2, {top: '50%', left: '50%', ease:Linear.easeNone})
-//         .to(sunAnimate, 2, {top: '100%', left: '100%', ease:Linear.easeNone});
-   
-// }   
+};
+
+var degrees = 0;
+        sunAnimate.onclick = function (){
+        degrees += 360;
+        TweenMax.fromTo(sunAnimate, 2, {scale: 0.5}, {scale: 1});
+    };
+    
+
+clock();    
+zeroAdded();
+animate(); 
+message();
 
 
+
+// var degrees = 0;
+// sunAnimate.onclick = function (){
+//     degrees += 360;
+//     TweenMax.to('#sun', 1, {rotationY: degrees});
+// };
 
 
 
