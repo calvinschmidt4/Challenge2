f4b3973c6631fa41dcd1be797f81c0ed1812ef04
calvinschmidt4 <calvinschmidt4@users.noreply.github.com>
2019-03-14 16:38:13 +0100

    Update tijd.js

--- javascript/tijd.js
+++ javascript/tijd.js
@@ -1,11 +1,6 @@
 var date, h, m, s, count;
 
-function load(){
-    clock();    
-    zeroAdded();
-    Animate(); 
-}    
-  
+
 function clock(){
     date = new Date();
     // de huidige uren, minuten en secondes worden opgevraagd.
@@ -19,8 +14,8 @@ function clock(){
     
     // de vorige waardes worden vervolgens in de div met id clock geplaatst.
     document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-    // background();
-    Animate(); 
+    message();
+    animate(); 
     count=setTimeout(clock,1000);
 }
 // de opgevraagde tijden worden gecontroleerd, wanneer een uur, minuut of seconde kleiner is dan 10 wordt daar een 0 aan vastgeplakt, zodat de klok er als een echte digitale klok uitziet.
@@ -28,68 +23,69 @@ function zeroAdded(i) {
     if (i < 10){
         i = "0" + i};
         return i;
-}
+};
 
-// // de achtergrond verandert afhankelijk van hoe laat het is. Dit gebeurt alleen op uur niveau. Bij een bepaald tijdstip wordt er aan body een class toegevoegd.
-// function background(){
-//     if(h > 19 || h < 6){
-//         document.body.className = "night";
-        
-//     } else if (h > 16 && h < 19){
-//         document.body.className = "sunset";
-//     } else {
-//         document.body.className = "day";
-//         document.getElementById("tekst").innerHTML = 'Day';
-        
-//     }
-// };
+// maakt een variabele aan met de id sun  
+var sunAnimate = document.getElementById("sun");
 
-// function Animate() {
-//     // maakt een variabele aan met de id sun    
-//     var sunAnimate = document.getElementById("sun");   
-   
-//     // als tijd tussen die uren is dan wordt de zon verplaatst voor een duratie van het aantal seconden dat het nog duurt tot het 20:00 is
-//     // als de tijd buiten die uren is dan wordt er een class toegevoegd waarmee de zon op display: none wordt gezet.
-//     if (h >= 6 && h <20){
-//         TweenMax.to(sunAnimate, (20 * 60 * 60) - ((h * 60 * 60) + (m * 60) + (s)), {bottom:'100%'});
-//     } else {
-//         var image = document.getElementById("sun");
-//         image.classList.add("sun_remove");
-//     }
-// }
-
-function Animate() {
-    // maakt een variabele aan met de id sun    
-    var sunAnimate = document.getElementById("sun");  
+function animate() {         
     // op basis van hoe laat het is wordt berekent hoeveel de zon zich moet verplaatsen 
-    // var moveValue = (h - 6) * (100 / 13);
-    var moveValue = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (100 / 14);
-    console.log(moveValue);
+    var moveValueUp = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (100 / 7);
+    var moveValueDown = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 13) * (100 / 7);
+    var moveValueRight = (date.getHours() + (date.getMinutes() / 60) + (date.getSeconds() / 3600) - 6) * (100 / 14);
     // als tijd tussen onderstaande uren is dan wordt de zon verplaatst over de afstand van moveValue in percentages
-    // als de tijd buiten die uren is dan wordt er een class toegevoegd waarmee de zon op display: none wordt gezet.
-      
-    if (h >= 6 && h <20){        
-        TweenMax.to(sunAnimate, 1, {bottom: moveValue +='%', scale: 0.5, rotationY: 1080});
-        if (h >= 6 && h <8){
-            document.getElementById("tekst").innerHTML = 'Sunrise';
-        } 
-        else if (h >= 8 && h <18){
-            document.getElementById("tekst").innerHTML = 'Day';   
-        }
-        else if (h >= 18 && h <20){
-            document.getElementById("tekst").innerHTML = 'Sunset';
-        }        
-        else {
-            document.getElementById("tekst").innerHTML = 'Night';
-        }        
-    } 
-    else {
-        var image = document.getElementById("sun");
-        image.classList.add("sun_remove");
+    // moveValueRight verplaats de zon constant naar rechts en moveValueUp en moveValueDown verplaatsen de zon eerst omhoog en dan omlaag.
+    // als de tijd buiten die uren is dan wordt er een class toegevoegd waarmee de zon op display: none wordt gezet en de achtergrond wordt zwart gemaakt.
+       
+    if (h >= 6 && h < 13){
+        TweenMax.to(sunAnimate, 0, {bottom: moveValueUp += '%', left: moveValueRight += '%'});
     }
-}
 
-load();
+    else if (h >= 13 && h < 20){
+        TweenMax.to(sunAnimate, 0, {top: moveValueDown += '%', left: moveValueRight += '%'});
+    }
+          
+    else {
+    var image = document.getElementById("sun");
+    image.classList.add("sun_remove");
+    var backgroundColor = document.getElementById("change_color");
+    backgroundColor.classList.add("night");
+    }
+};
+
+// tussen bepaalde uren wordt er tekst in de zwarte blak geplakt afhankelijk van de fase van de dag
+function message(){                            
+    if (h >= 6 && h <8){
+        document.getElementById("tekst").innerHTML = 'Sunrise';
+    } 
+    else if (h >= 8 && h <18){
+        document.getElementById("tekst").innerHTML = 'Day';   
+    }
+    else if (h >= 18 && h <20){
+        document.getElementById("tekst").innerHTML = 'Sunset';
+    }        
+    else {
+        document.getElementById("tekst").innerHTML = 'Night';
+    }        
+
+};
+
+//bij een onclick op de zon gaat die naar de helft van de grootte en vervolgens weer terug naar de normale grootte
+var degrees = 0;
+sunAnimate.onclick = function (){
+    degrees += 360;
+    TweenMax.fromTo(sunAnimate, 2, {scale: 0.5}, {scale: 1});
+};
+    
+
+clock();    
+zeroAdded();
+animate(); 
+message();
+
+
+
+
 
 
 
